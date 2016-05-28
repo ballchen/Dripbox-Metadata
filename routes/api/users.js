271c383e397a1e8c895065934beb4ec54884bc40
ballchen <kerkerball@gmail.com>
2016-05-28 15:51:16 +0800

    fix register

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,7 +21,7 @@ const loginSchema = Joi.object().keys({
 exports.register = function *() {
   const form = yield Joi.validateAsync(this.request.body, registerSchema)
 
-  let user = yield this.mongo.collection('user').findOne(form).sort()
+  let user = yield this.mongo.collection('user').findOne(form)
 
   if (user) {
     this.status = 400
